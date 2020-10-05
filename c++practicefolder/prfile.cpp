277fd6372356b197a208513f745cd0139345a0ff
jaffarabbas <gamaportal8@gmail.com>
2020-10-05 22:07:10 +0500

    5-10-2020

---
+++ c++practicefolder/prfile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define mall(L,B) (L+B)
+
+#include "helper.cpp"
+
+#include "conditional.cpp"
+
+
+int main(){
+	 std::cout << "Area of rectangle is: " << mall(2,3); 
+	 std::cout << "header is: " << check(5,3); 
+	  std::cout << "sdfdsf" << cou(); 
+}
