c29119b2fffbc849fb91eb675149346a62415e93
jaffarabbas <gamaportal8@gmail.com>
2020-10-07 18:50:13 +0500

    10-7-2020

---
+++ OOP/encapsulation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class encap{
+    private: int a;//private variables
+
+    public:
+    int set(int x){
+        a = x;
+    }
+
+    int get(){
+        return a;
+    }
+};
+
+class tester{
+    public:
+      void show(){
+         encap obj;
+         int temp;
+         cin >> temp;
+         obj.set(temp);
+         cout << obj.get() << endl;
+      }
+};
+
+int main(int argc, char const *argv[])
+{
+    tester test;
+    test.show();
+    return 0;
+}
