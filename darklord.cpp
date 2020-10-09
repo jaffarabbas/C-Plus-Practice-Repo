554fc130afc7956d12c708791b94a75e1131cc54
jaffarabbas <gamaportal8@gmail.com>
2020-10-09 08:02:58 +0500

    10-9-2020

--- darklord.cpp
+++ darklord.cpp
@@ -2,9 +2,26 @@
 
 //have any function
 
-
 #include <iostream>
 using namespace std;
 
-
-int 
+//integer
+int inscn(){
+    int num;
+    cin >> num;
+}
+//float
+float flscn(){
+    float num;
+    cin >> num;
+}
+//double
+double doscn(){
+    double num;
+    cin >> num;
+}
+//char
+char chscn(){
+    char ch;
+    cin >> ch;
+}
