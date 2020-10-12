aa57a11be1e3bfbf781e8429b08d14f193c52a3a
jaffarabbas <gamaportal8@gmail.com>
2020-10-12 17:18:44 +0500

    Update GFG.java

--- c++practicefolder/GFG.java
+++ c++practicefolder/GFG.java
@@ -10,18 +10,18 @@ class GFG {
 	    String number = scn.next();
 	   // if(number.lenth() >=1 && <=20){
 	            if(number.length()== 10){
-	                if(number.charAt(0) == '7' && number.charAt(0) == '8' && number.charAt(0) == '9'){
+	                if(number.charAt(0) == '7'|| number.charAt(0) == '8'|| number.charAt(0) == '9'){
 	                    System.out.println("Valid");
 	                }
 	            }else if(number.length()== 11){
 	                if(number.charAt(0) == '0'){
-	                    if(number.charAt(1) == '7' && number.charAt(1) == '8' && number.charAt(0) == '1'){
+	                    if(number.charAt(1) == '7'|| number.charAt(1) == '8'|| number.charAt(0) == '1'){
 	                    System.out.println("Valid");
 	                    }
 	                }
 	            }else if(number.length()== 12){
 	                if(number.charAt(0) == '9' || number.charAt(1) == '1'){
-	                    if(number.charAt(0) == '7' && number.charAt(0) == '8' && number.charAt(0) == '9'){
+	                    if(number.charAt(0) == '7'|| number.charAt(0) == '8'|| number.charAt(0) == '9'){
 	                    System.out.println("Valid");
 	                    }
 	                }
