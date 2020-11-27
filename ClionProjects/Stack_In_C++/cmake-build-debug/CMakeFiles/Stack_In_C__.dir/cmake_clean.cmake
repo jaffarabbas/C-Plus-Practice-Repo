f11284a107cd79d3013beb7bd3d11081a5be7ec6
jaffarabbas <gamaportal8@gmail.com>
2020-11-27 15:32:02 +0500

    27-11-2020     Stack Project

---
+++ ClionProjects/Stack_In_C++/cmake-build-debug/CMakeFiles/Stack_In_C__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stack_In_C__.dir/main.cpp.obj"
+  "Stack_In_C__.exe"
+  "Stack_In_C__.exe.manifest"
+  "Stack_In_C__.pdb"
+  "libStack_In_C__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stack_In_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
