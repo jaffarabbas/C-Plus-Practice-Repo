65f6fe8b06a1723f9a3ae98b5a0b8bed6672faf6
jaffarabbas <gamaportal8@gmail.com>
2021-02-06 12:01:20 +0500

    2-6-2021

---
+++ c++practicefolder/expo.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    int max = 2e5;
+    for (int i = 0; i < max; i++){
+        cout << i << endl;
+    }
+	return 0;
+}
