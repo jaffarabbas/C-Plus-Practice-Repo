b8db3bf601ccbbbbe97d1856f7822a9dc0551aef
jaffarabbas <gamaportal8@gmail.com>
2021-02-14 19:02:39 +0500

    2-14-2021

--- C_Practice/codes/checknboiler.cpp
+++ C_Practice/codes/checknboiler.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+
+void sort(int arr[]){
+	int i,j,temp;
+	for(i=0;i<5;i++){
+		for(j=0;j<(5-i)-1;j++){
+			if(arr[j] > arr[j+1]){
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
+
+
+
 int main(){
-	printf("sadas");
+	int arr[] = {5,4,3,2,1};
+	int i;
+	for(i=0;i<5;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	sort(arr);
+		for(i=0;i<5;i++){
+		printf("%d ",arr[i]);
+	}
 	return 0;
 }
