f853d4bf0495212c934236a850cf2de3221f3e97
jaffarabbas <gamaportal8@gmail.com>
2021-03-20 21:35:17 +0500

    3-20-2021

---
+++ c++practicefolder/pipe_work.cpp
@@ -0,0 +1,19 @@
+// C++ program to demonstrate calculation in parent and 
+// child processes using fork() 
+#include <iostream> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <bits/stdc++.h> 
+#include <stdlib.h>
+using namespace std; 
+
+// Driver code 
+int main() 
+{
+    Test();
+    return 0; 
+} 
+
+void Test(){
+  
+}
