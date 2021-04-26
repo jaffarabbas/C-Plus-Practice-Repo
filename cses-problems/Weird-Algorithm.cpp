ee42f868a7787557451936ef38ce28e92bd2d350
jaffarabbas <gamaportal8@gmail.com>
2021-04-26 21:02:01 +0500

    4-26-2021   Weird Algorithum

--- cses-problems/Weird-Algorithm.cpp
+++ cses-problems/Weird-Algorithm.cpp
@@ -1,18 +1,73 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <ext/pb_ds/tree_policy.hpp>
 
-int main(int argc, char const *argv[]){
-    int a;
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+// change these numbers
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 100001;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    ll a;
     cin >> a;
-    cout<< a << " ";
-    while(true){
-        if(a!=1){
+    cout << a << " "; 
+    while(a!=1){
             if(a%2==0){
                 a /= 2;
             }else if(a%2!=0){
-                a =(a * 3)+1;
+                a = a * 3+1;
             }
-            cout<< a << " ";
-        }
+           cout << a << " "; 
     }
-} 
+    return 0;
+}
+
+// read the question correctly (ll vs int)
+// template by bqi343 and iNum
+// used by techguy2
+// functions go before int main() silly
+
+
+
+
+
+
+
+
+
+
+
+
+
+
