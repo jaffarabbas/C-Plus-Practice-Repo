18d970d36b27e8305f396f2788ccd1b54849870f
jaffarabbas <gamaportal8@gmail.com>
2021-04-27 15:17:02 +0500

    4-27-2021

---
+++ cses-problems/missing_number.cpp
@@ -0,0 +1,55 @@
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+//template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+// change these numbers
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 100001;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen("J:\\Program\\Github\\C-Plus-Practice-Repo\\cses-problems\\filesio\\input.txt", "r", stdin); freopen("J:\\Program\\Github\\C-Plus-Practice-Repo\\cses-problems\\filesio\\output.txt", "w", stdout); 
+    ll a;
+    cin >> a;
+    // ll arr[a];
+    // FOR(i,0,a){
+    //     cin >> arr[i];
+    // }
+    cout << a << " ";
+    return 0;
+}
