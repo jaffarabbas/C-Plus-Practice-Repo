7eb0a72058f5590e9e89bfcc4b9fcc44a2dd4269
jaffarabbas <gamaportal8@gmail.com>
2021-10-09 13:15:57 +0500

    09/10/2021 13:15:57

---
+++ Compitative-Programming/CodeCheff/Find-Reminder.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void testCode(){
+	int x,y;
+	cin>>x>>y;
+	cout<<x%y<<endl;
+}
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		testCode();
+	}
+	return 0;																								
+}																																																							
