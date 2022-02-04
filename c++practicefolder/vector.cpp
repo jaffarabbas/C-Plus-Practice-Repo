6219bec9067037abcb8075c4030577a0e7031f15
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-04 22:17:06 +0500

    04/02/2022 22:17:04

--- c++practicefolder/vector.cpp
+++ c++practicefolder/vector.cpp
@@ -2,27 +2,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isEmpty(vector<int> &v){
-    return v.size() <= 0;
-}
-
-void insert(vector<int> &v,int a){
-    return v.push_back(a);
-}
-
-void print(vector<int> &v){
-    for(int i=0;i<v.size();i++){
-        cout << v[i] << " ";
-    }
-}
-
-
-
-
-
 int main(int argc, char const *argv[]){
-    vector<int> vec;
-    // insert(vec,3);
-    // print(vec);
-    cout << isEmpty(vec);
+    int n;
+    cin >> n;
+    cout << n+1;
+    return 0;
 }
