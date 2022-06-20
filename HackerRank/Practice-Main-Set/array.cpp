86a755cb6da23bbfb42440d146f2167f4b8ab295
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-20 21:13:45 +0500

    Inserted Practice-Main-Set/array.cpp at 20/06/2022 21:13:45

---
+++ HackerRank/Practice-Main-Set/array.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,a;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d",&a);
+        arr[i] = a;
+    }
+    for (int i = n - 1; i >= 0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
