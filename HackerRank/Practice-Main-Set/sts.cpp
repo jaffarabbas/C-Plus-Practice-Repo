3adf897ec9955c2093266dd0d8c32747258ad150
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-23 21:51:52 +0500

    Inserted Practice-Main-Set/sts.cpp at 23/06/2022 21:51:52

---
+++ HackerRank/Practice-Main-Set/sts.cpp
@@ -0,0 +1,25 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+   stringstream s(str);
+   char ch;
+   int a;
+   vector<int> v;
+   while(s >> a >> ch){
+    v.push_back(a);
+   }
+   return v;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    return 0;
+}
