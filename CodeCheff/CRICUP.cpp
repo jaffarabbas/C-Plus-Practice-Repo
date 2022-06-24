5e7f6c4dc9ffda38a215eeee73b55fa0d3cb63dd
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-24 23:36:18 +0500

    Inserted CRICUP.cpp at 24/06/2022 23:36:18

---
+++ CodeCheff/CRICUP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin >> a >> b;
+	if(a == 0 && b == 1){
+		cout << "https://www.codechef.com/practice";
+	}else if(a == 1 && b == 0){
+		cout << "https://www.codechef.com/contests";
+	}else if(a == 1 && b == 1){
+		cout << "https://discuss.codechef.com";
+	}
+	return 0;
+}
